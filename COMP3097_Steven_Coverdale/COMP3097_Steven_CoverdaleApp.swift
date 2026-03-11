51c8c49e795114968d2de65fb6ad2fb3b1659205
StevenCoverdale <S.coverdale@hotmail.com>
2026-03-11 19:45:49 -0400

    Initial Commit

---
+++ COMP3097_Steven_Coverdale/COMP3097_Steven_CoverdaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  COMP3097_Steven_CoverdaleApp.swift
+//  COMP3097_Steven_Coverdale
+//
+//  Created by steven coverdale on 2026-03-11.
+//
+
+import SwiftUI
+
+@main
+struct COMP3097_Steven_CoverdaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ COMP3097_Steven_Coverdale/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  COMP3097_Steven_Coverdale
+//
+//  Created by steven coverdale on 2026-03-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
