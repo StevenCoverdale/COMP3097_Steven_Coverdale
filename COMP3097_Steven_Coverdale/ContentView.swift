9fd8a6559e75bd94d19d59aed8ed41faf2a8ba5c
StevenCoverdale <S.coverdale@hotmail.com>
2026-03-15 14:54:59 -0400

    Added a simple answer handler

--- COMP3097_Steven_Coverdale/ContentView.swift
+++ COMP3097_Steven_Coverdale/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var number = Int.random(in: 1...200)
+    @State private var lastAnswerCorrect: Bool? = nil
     
     var body: some View {
         VStack(spacing: 30) {
@@ -19,19 +20,29 @@ struct ContentView: View {
             Text("\(number)")
                 .font(.system(size: 80, weight: .bold))
             
+            if let result = lastAnswerCorrect {
+                Text(result ? "Correct" : "Wrong")
+                    .font(.title2)
+            }
+            
             HStack(spacing: 40) {
                 Button("Prime") {
-                    let _ = isPrime(number)
+                    handleAnswer(isPrime(number))
                 }
                 .font(.title)
                 
                 Button("Not Prime") {
-                    let _ = !isPrime(number)
+                    handleAnswer(!isPrime(number))
                 }
                 .font(.title)
             }
         }
         .padding()
+    }
+    
+    func handleAnswer(_ isCorrect: Bool) {
+        lastAnswerCorrect = isCorrect
+        number = Int.random(in: 1...200)
     }
     
     func isPrime(_ n: Int) -> Bool {
